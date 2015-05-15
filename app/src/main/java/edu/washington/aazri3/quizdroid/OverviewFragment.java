ad587634483528fb666e4b62a9c790bc9e450d36
Azri Azmi <azri92@gmail.com>
2015-05-15 06:10:22 -0700

    Add image on Overview screen. Finally done.

--- app/src/main/java/edu/washington/aazri3/quizdroid/OverviewFragment.java
+++ app/src/main/java/edu/washington/aazri3/quizdroid/OverviewFragment.java
@@ -1,12 +1,15 @@
 package edu.washington.aazri3.quizdroid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -14,6 +17,7 @@ import android.widget.TextView;
  * Display quiz overview.
  */
 public class OverviewFragment extends Fragment {
+    private static final String TAG = "OverviewFragment";
 
     QuizFragmentInterface mCallback;
 
@@ -24,8 +28,15 @@ public class OverviewFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        String topic = QuizApp.getInstance().getRepository().getTopic();
+
+        ImageView imgIcon = (ImageView) getActivity().findViewById(R.id.topicIcon);
+        String id = QuizApp.getInstance().getRepository().getIcon(topic);
+        int imgID = getResources().getIdentifier(id, "mipmap", getActivity().getPackageName());
+        imgIcon.setImageResource(imgID);
+
         TextView txtTopic = (TextView) getActivity().findViewById(R.id.txtTopic);
-        txtTopic.setText(QuizApp.getInstance().getRepository().getTopic());
+        txtTopic.setText(topic);
 
         TextView txtDescription = (TextView) getActivity().findViewById(R.id.txtDescription);
         txtDescription.setText(QuizApp.getInstance().getRepository().getLongDesc());
@@ -37,11 +48,13 @@ public class OverviewFragment extends Fragment {
                 mCallback.onButtonClicked("begin");
             }
         });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_overview, container, false);
     }
 
